b995e037f44c88e005bcb9bed452bf1fe7053793
cdg1921 <39193906+cdg1921@users.noreply.github.com>
2018-05-11 22:19:21 +0800

    stock profit or loss analyse in R
    DESCRIPTION
    Here we have a list of stock orders for some users in the same period. We want to sort these users according to their realized profit, i.e. how much they have already earned. 
    
    Please note that:
    1.	Either long a stock (buy first, and sell later) or short a stock (sell first, and buy later) first is allowed
    2.	For a trader, total Buy Quantity and Sell Quantity might not be equal, in that case, just calculate the realized profit or loss
    3.	Transaction fee should be considered as cost, and all fees are realized
    
    INPUT
    Input is in TSV format, i.e. columns are separated by tab, like:
    
    OrderId	Trader	StkCode	Quantity	Price	TradeType	Fee	Timestamp
    1	AAA	222222	100	10	Buy	0.2	2016-05-09 13:38:24
    2	BBB	333333	1000	8.8	Sell	10.56	2016-05-09 10:37:16
    3	AAA	222222	100	11	Sell	1.32	2016-05-10 13:38:24
    4	BBB	333333	800	9.0	Buy	1.6	2016-05-11 14:58:30
    5	BBB	333333	400	9.2	Buy	0.8	2016-05-16 10:28:24
    6	AAA	555555	600	15.5	Buy	2.0	2016-05-16 10:31:33
    7	...
    
    OUTPUT
    Output should be TSV format too, including 2 columns: Trader, profit/loss. Header is not needed. 
    
    For example, given the input above, the correct output is:
    AAA	96.48
    BBB	-252.96	
    
    REQUIREMENT
    Please write code that takes orders as input, and outputs users’ profits, sorted in descending order. 
    You may use any language you like. Your solution will be graded on the following criteria:
    •	Code  readability, quality, and design. In terms of quality, please write your code as if it would eventually be pushed into production.
    •	Correctness / performance (speed/memory footprint).

---
+++ Stock_profit_or_loss_analyse.r
@@ -0,0 +1,106 @@
+# import data
+data_stock = read.table("D:/data.tsv",header = TRUE, sep = "\t")  #Suppost the data file located in 'D:/'
+
+#Transfer the Timestamp to valid format,for example, from  2016\\5\\9 13:38 to 2016-05-09 13:38:00
+data_stock$Timestamp <- gsub("[\\]","-",data_stock$Timestamp) 
+data_stock$Timestamp <- as.POSIXlt(data_stock$Timestamp)
+
+#Loss = buy + fee; Profit = sell - fee
+for (i in 1:nrow(data_stock)) { # i = 1
+    if(data_stock[i,c('TradeType')] == 'Buy'){
+      data_stock[i,c('money')] <- data_stock[i,c('Quantity')] * data_stock[i,c('Price')] + data_stock[i,c('Fee')]
+    }
+    else{
+      data_stock[i,c('money')] <- data_stock[i,c('Quantity')] * data_stock[i,c('Price')] - data_stock[i,c('Fee')]
+    }
+}
+
+data_stock <- data.frame(data_stock)
+data_stock <- data_stock[order(data_stock[,2],data_stock[,8]),]
+
+#Sell detail
+data_sell <- data_stock[which(data_stock$TradeType %in% c('Sell')),]
+
+#Buy detail(the stock has been selled)
+data_buy <- data_stock[which((data_stock$TradeType %in% c('Buy'))& (data_stock$StkCode %in% c(data_sell$StkCode))),]
+
+#Buy detail(the stock has never been selled)
+data_buy_and_not_sell <- subset(data_stock[which((data_stock$TradeType %in% c('Buy'))& !(data_stock$StkCode %in% c(data_sell$StkCode))),], select = c(Trader,Fee))
+
+
+data_buy_and_sell <- aggregate(x=data_sell[c('Quantity','money')], by = list(data_sell$Trader, data_sell$StkCode), FUN=sum)
+
+#Initial the varibles to 0
+data_buy_and_sell$Buy_Quantity_sum <- 0
+data_buy_and_sell$Buy_money_sum <- 0   
+data_buy_and_sell$profit_or_loss <- 0     #profit/loss
+
+#Rename col name
+colnames(data_buy_and_sell)[1:2]<-c("Trader","StkCode")
+
+#buy and sell,profit/loss  include the trade profit/loss and fee
+if(nrow(data_buy)==0){
+  print('NO BUY TRADE!!')
+} else if(nrow(data_buy_and_sell)==0){
+  print('NO SELL TRADE!!')
+}else{
+  for(i in 1:nrow(data_buy)){# i = 1
+    for (j in 1:nrow(data_buy_and_sell)) {# j = 1
+      if((data_buy[i,c('Trader')] == data_buy_and_sell[j,c('Trader')])&(data_buy[i,c('StkCode')] == data_buy_and_sell[j,c('StkCode')])){ #同一用户买同一只股票
+        #money_last <- data_buy_and_sell[j,c('Buy_money_sum')]       #截止至上一次的交易额
+        Quantity_last <- data_buy_and_sell[j,c('Buy_Quantity_sum')] #截止至上一次的交易量
+        
+        data_buy_and_sell[j,c('Buy_Quantity_sum')] = data_buy_and_sell[j,c('Buy_Quantity_sum')] + data_buy[i,c('Quantity')]
+        data_buy_and_sell[j,c('Buy_money_sum')] = data_buy_and_sell[j,c('Buy_money_sum')] + data_buy[i,c('money')]
+        
+        if(data_buy_and_sell[j,c('Buy_Quantity_sum')] < data_buy_and_sell[j,c('Quantity')]){
+          next()
+        }
+        else if(data_buy_and_sell[j,c('Buy_Quantity_sum')] == data_buy_and_sell[j,c('Quantity')]){
+          data_buy_and_sell[j,c('profit_or_loss')]= data_buy_and_sell[j,c('money')] - data_buy_and_sell[j,c('Buy_money_sum')]
+        }
+        else if(data_buy_and_sell[j,c('Buy_Quantity_sum')] > data_buy_and_sell[j,c('Quantity')]& Quantity_last < data_buy_and_sell[j,c('Quantity')]){
+          over_money <- (data_buy_and_sell[j,c('Buy_Quantity_sum')] - data_buy_and_sell[j,c('Quantity')])*data_buy[i,c('Price')]
+          data_buy_and_sell[j,c('profit_or_loss')]= data_buy_and_sell[j,c('money')] - data_buy_and_sell[j,c('Buy_money_sum')] + over_money
+        }
+        else{
+          data_buy_and_sell[j,c('profit_or_loss')]= data_buy_and_sell[j,c('profit_or_loss')] - data_buy[i,c('Fee')]  #后面买入，还没有卖出的股票
+        }
+        
+      }
+    }
+  }
+  #Just keep Trader and profit_or_loss
+  data_buy_and_sell <- subset(data_buy_and_sell, select=c(Trader,profit_or_loss))
+}
+
+
+#If buy and never save the stock, the profit_or_loss is just the fee
+data_profit <- merge(data_buy_and_sell,data_buy_and_not_sell,by='Trader',all.x = TRUE)
+data_profit[is.na(data_profit)] <- 0
+data_profit$profit_or_loss <- data_profit$profit_or_loss - data_profit$Fee
+data_profit <- subset(data_profit, select=c(Trader,profit_or_loss))
+
+
+#Remove the header
+dimnames(data_profit) <- list(1:nrow(data_profit), 1:ncol(data_profit))  
+
+#Save the result
+write.table(data_profit,file = "D:/data_result.tsv", col.name = FALSE, quote = FALSE, sep = "\t",row.names = FALSE)
+
+#clear invalid datas and varibles
+#rm(data_buy_and_not_sell,data_buy_and_sell,data_stock,data_profit)
+rm(list = ls())   #clear the environment
+
+#Reload and print the result
+data_stock1 = read.table("D:/data_result.tsv",header = TRUE, sep = "\t")
+print(data_stock1)
+#Results will be shown as follow:
+#   X1      X2
+#1 AAA   96.48
+#2 BBB -252.96
+#Note:in R,if a table doesn't include headers， X1,X2,……will be default
+
+#Finally,We can put the script file in 'D:/', and then can run the script through the following script:
+#system.time(source("D:/Stock_profit_or_loss_analyse.r"))
+#add analyse
